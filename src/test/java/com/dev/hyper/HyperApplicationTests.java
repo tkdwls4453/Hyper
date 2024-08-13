698fb8fad677d5e9dddb9a63eafaab400e27fde0
Yoon321 <tkdwls734@gmail.com>
2024-08-13 10:48:23 +0900

    [FEAT] #2 스프링 부트 프로젝트 생성
    초기 의존성 셋팅
    - lombok, spring web, h2database

---
+++ src/test/java/com/dev/hyper/HyperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.hyper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
