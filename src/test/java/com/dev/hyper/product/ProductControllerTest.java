4a3d2cb5b25e94ba4f24b971c39eeae24e01c8f9
Yoon321 <tkdwls734@gmail.com>
2024-08-15 09:59:08 +0900

    [TEST] #10 제품 컨트롤러 테스트 실패 해결
    productService 를 목빈으로 등록하여 해결

--- src/test/java/com/dev/hyper/product/ProductControllerTest.java
+++ src/test/java/com/dev/hyper/product/ProductControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -21,6 +22,9 @@ class ProductControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private ProductService productService;
 
     @Autowired
     private ObjectMapper objectMapper;
